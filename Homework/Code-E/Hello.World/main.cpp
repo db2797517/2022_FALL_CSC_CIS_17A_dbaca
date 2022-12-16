b392af8e5974090bec4ef26a7f4bb89ecb9e45e5
Daniel Baca <85714544+db2797517@users.noreply.github.com>
2022-12-16 00:45:20 -0800

    Add files via upload

---
+++ Homework/Code-E/Hello.World/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * Author: Daniel Baca
+ * Date: 9/3/2022
+ * Purpose: Output "Hello World" to console.
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Constants like PI, e, Gravity, Conversions, 2D array size only!
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    
+    // Output to console
+    cout << endl << "   Hello World!  " << endl << endl;
+
+    //Exit
+    return 0;
+}
