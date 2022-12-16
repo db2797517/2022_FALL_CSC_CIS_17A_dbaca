9b28dd56246563dd5468d7ebfbca94365702eaaf
Daniel Baca <85714544+db2797517@users.noreply.github.com>
2022-12-16 00:43:39 -0800

    Add files via upload

---
+++ Homework/Intro Lab/Hello_World_Prg/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Daniel Baca
+ * Created on September 1st, 2022, 8:33 PM
+ * Purpose:  Output from console, "Hello World."
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output Library
+using namespace std;  //STD Name-space where Library is compiled
+
+//Code Begins Execution Here with function main
+int main(int argc, char** argv) 
+{
+    //Display the results 
+    cout << "Hello World!" << endl;
+
+    //Exit
+    return 0;
+}
+
